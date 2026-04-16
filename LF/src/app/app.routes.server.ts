ceb20a6a2b11a106f3af5c4ae74045c7d878ffc8
JulsZero <julianxrz@hotmail.com>
2026-04-15 22:09:45 -0500

    workflow test 2

--- LF/src/app/app.routes.server.ts
+++ LF/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'admin/reportes/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'publicaciones',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
